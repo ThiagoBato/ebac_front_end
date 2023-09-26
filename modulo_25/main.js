ee0a604961266806d9abd20aa6f45071ea3f14aa
Thiago Oliveira Vieira <thiago.ov@hotmail.com>
2023-09-25 21:02:42 -0300

    Refactoring the code so it only needs the .map method to work properly

--- modulo_25/main.js
+++ modulo_25/main.js
@@ -14,13 +14,9 @@ const alunos = [
     },
 ];
 
-//Método para filtrar apenas os alunos com nota igual ou maior do que 6;
-const aprovados = alunos.filter((aluno) => {
-    return aluno.nota >= 6;
-});
-//Método para colocar os alunos aprovados em um array e não em um objeto, como estávam alocados na variável aprovados;
-const alunosAprovados = aprovados.map((aluno) => {
-    return `${aluno.nome} com a nota ${aluno.nota}`;
+//Método para filtrar apenas os alunos com nota igual ou maior do que 6 e ;
+const alunosAprovados = alunos.map((aluno) => {
+    return aluno.nota >= 6 ? `${aluno.nome} com a nota ${aluno.nota}` : null;
 });
 //Método para mostrar os alunos aprovados, com uma separação de vírgula, espaço e quebra de linha entre eles.
 console.log(`Os alunos aprovados são:\n ${alunosAprovados.join(",\n ")}`);
