6b7bda2db66bdaba4cf6956e725b09b877aa6c41
Thiago Oliveira Vieira <thiago.ov@hotmail.com>
2023-09-25 22:11:18 -0300

    Changing the exercise to use .filter insted of .map, as the tutor asked

--- modulo_25/main.js
+++ modulo_25/main.js
@@ -14,9 +14,21 @@ const alunos = [
     },
 ];
 
+//Método utilizando .map
+/*
 //Método para filtrar apenas os alunos com nota igual ou maior do que 6 e ;
 const alunosAprovados = alunos.map((aluno) => {
     return aluno.nota >= 6 ? `${aluno.nome} com a nota ${aluno.nota}` : null;
 });
 //Método para mostrar os alunos aprovados, com uma separação de vírgula, espaço e quebra de linha entre eles.
 console.log(`Os alunos aprovados são:\n ${alunosAprovados.join(",\n ")}`);
+*/
+
+//Método utilizando .filter e forEach para mostrar o resultado
+const aprovados = alunos.filter((aluno) => {
+    return aluno.nota >= 6;
+});
+console.log("Os alunos aprovados são: ");
+aprovados.forEach((aprovado) => {
+    console.log(`${aprovado.nome} com a nota ${aprovado.nota}`);
+});
