402ae625d32617d57c153182116faef6b58a9371
Thiago Oliveira Vieira <thiago.ov@hotmail.com>
2023-09-30 17:24:10 -0300

    Adding the Module 27 exercise - VueJS

---
+++ modulo_27/module-27/src/App.vue
@@ -0,0 +1,24 @@
+
+
+<script setup>
+import Header from './components/Header.vue';
+import Calculator from './components/Calculator.vue';
+</script>
+
+<template>
+  <header class="pt-5">
+    <div class="container">
+      <Header />
+
+    </div>
+  </header>
+  <main class="pt-5">
+    <div class="container">
+      <Calculator />
+    </div>
+  </main>
+</template>
+
+<style scoped>
+
+</style>
