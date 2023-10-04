2f7f0c7032aa2103730627be546d46a3b92c152c
Thiago Oliveira Vieira <thiago.ov@hotmail.com>
2023-10-03 21:55:49 -0300

    Adding the Module 28 exercise - Introduction to React

---
+++ modulo_28/calculadora-imc/src/App.jsx
@@ -0,0 +1,15 @@
+import Calculadora from './components/CalculadoraIMC'
+import Header from './components/Header'
+import './main.css'
+
+function App () {
+  
+  return (
+    <>
+      <Header />
+      <Calculadora/>
+    </>
+  )
+}
+
+export default App
---
+++ modulo_28/calculadora-imc/src/components/Header/index.jsx
@@ -0,0 +1,13 @@
+import './header.css'
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <div className="container">
+                <h1 className='title'>Calculadora de IMCS</h1>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
